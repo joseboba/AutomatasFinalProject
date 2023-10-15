e8caf65a4d96300f610a66cdbccec3411ae6bad1
jbarahona02 <jbarahona-2018064@kinal.edu.gt>
2023-10-15 16:45:54 -0600

    Add interface to show result

--- src/main/java/com/umg/Main.java
+++ src/main/java/com/umg/Main.java
@@ -1,30 +1,50 @@
 package com.umg;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import com.umg.service.FileService;
+import com.umg.ui.PantallaResultado;
 import com.umg.util.Utilities;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    
 
+    public static void main(String[] args) throws IOException {
+        inicioDeAplicacion();
+    }
+    
+    public static void inicioDeAplicacion() throws IOException {
+        PantallaResultado pantallaResultado = new PantallaResultado();
+        ArrayList<String> resultadoLecturaArchivo =  lecturaDeArchivo();
+        if(resultadoLecturaArchivo != null){
+            pantallaResultado.asignacionDeResultado(resultadoLecturaArchivo);
+            pantallaResultado.setTitle("Analizador léxico");
+            pantallaResultado.setVisible(true);
+        }
+    }
+    
+    public static ArrayList<String> lecturaDeArchivo() throws IOException {
         Utilities.enterMapValues();
         var fileService = new FileService();
         var results = fileService.processFile();
+        ArrayList<String> resultadoAMostrar = new ArrayList();
 
-
-        for (var result : results) {
-            System.out.println("--------------------");
-            System.out.println("Sentencia: \n" + result.getOriginalLine());
-            if(!result.getTokens().isEmpty()){
-                    System.out.println("\nToken:");
-                    result.getTokens().forEach(t -> {
-                    System.out.println(t.getValue() + " \t" +  Utilities.getWordClassification(t.getType()));
-                });
-            }
-            System.out.println("--------------------\n");
+        if(!results.isEmpty()){
+           for (var result : results) {
+                resultadoAMostrar.add("\n ------------------------------------------- ");
+                resultadoAMostrar.add("\n Sentencia: \n\n " + result.getOriginalLine() + "\n");
+                if(!result.getTokens().isEmpty()){
+                        resultadoAMostrar.add("\n Token: \n");
+                        result.getTokens().forEach(t -> {
+                        resultadoAMostrar.add("\n " + t.getValue() + " \t" +  Utilities.getWordClassification(t.getType()));
+                    });
+                }
+                resultadoAMostrar.add("\n ------------------------------------------- \n");
+            } 
+          return resultadoAMostrar;
         }
-
+        
+        return null;
 
     }
 }
